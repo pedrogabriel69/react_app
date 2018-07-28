b5d6183de3e1cee0f10df1843a07474bbeda7af1
erisnuts <eris909@mail.ru>
2018-07-28 15:40:53 +0500

    [fix] renamed constant file Products -> products; real images added

--- src/components/Image.js
+++ src/components/Image.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 const Image = ({src, alt, height, width}) => (
   <img src={src} alt={alt} height={height} width={width} />
-)
+);
 
 export default Image;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from './Image';
-import TextBox from './Textbox';
+import Textbox from './Textbox';
 import Price from './Price';
 
 const ProductCard = ({product}) => {
@@ -12,7 +12,7 @@ const ProductCard = ({product}) => {
         <Image src={imageUrl} alt={imageContext} height={imageHeight} width={imageWidth} />
       </div>
       <div className="text-box">
-        <TextBox context={product.context} />
+        <Textbox context={product.context} />
       </div>
       <div className="price-value">
         <Price priceValue={product.priceValue} />
